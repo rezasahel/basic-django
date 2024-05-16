619744138a75a53651a2ae70f4b5fed250cbbf91
Reza Sahel <mrezasahel@gmail.com>
2024-05-16 22:45:42 +0200

    bootstrap templates added

--- message/views.py
+++ message/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Message
 
 
-class MessageView(TemplateView):
+class MessageView(ListView):
+    model = Message
     template_name = 'home.html'
     
